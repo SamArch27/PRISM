c97e076db7e349c8b6c3447c4570003fc99f22e0
sarch <sam.arche912912@gmail.com>
2024-03-13 18:40:20 -0400

    Correctly compute predicate per path

--- udf_transpiler/src/predicate_analysis.cpp
+++ udf_transpiler/src/predicate_analysis.cpp
@@ -98,12 +98,38 @@ void PredicateAnalysis::runAnalysis() {
         auto pathsToReturn = getAllPathsToBlock(&block);
 
         for (auto &path : pathsToReturn) {
+          String cond = "";
+
           std::cout << "Path: ";
+          BasicBlock *prevBlock = nullptr;
           for (auto *block : path) {
             std::cout << block->getLabel() << " ";
+            if (auto *branch =
+                    dynamic_cast<const BranchInst *>(block->getTerminator())) {
+              if (branch->isConditional()) {
+                if (cond != "") {
+                  cond += " AND ";
+                }
+
+                cond += "(";
+
+                if (prevBlock != branch->getIfFalse()) {
+                  cond += "NOT";
+                }
+                cond += "(" + branch->getCond()->getRawSQL() +
+                        ") IS DISTINCT FROM TRUE";
+                cond += ")";
+              }
+              prevBlock = block;
+            }
           }
+
+          auto boundCondition = f.bindExpression(cond);
           std::cout << std::endl;
+          std::cout << "Predicate: " << *boundCondition << std::endl;
         }
+
+        // Collect all conjunctions along the path
       }
     }
   }
