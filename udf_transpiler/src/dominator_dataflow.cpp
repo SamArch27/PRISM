336ff0beaa2042bebe8f8974098b00907a2fa8ac
sarch <sam.arche912912@gmail.com>
2024-03-13 09:34:49 -0400

    Working dominator tree

--- udf_transpiler/src/dominator_dataflow.cpp
+++ udf_transpiler/src/dominator_dataflow.cpp
@@ -39,9 +39,106 @@ void DominatorAnalysis::genBoundaryInner() {
 }
 
 void DominatorAnalysis::finalize() {
+
+  Map<BasicBlock *, BasicBlock *> idoms;
+  for (auto &block : f) {
+    idoms[&block] = nullptr;
+  }
+  idoms[f.getEntryBlock()] = f.getEntryBlock();
+
+  Map<BasicBlock *, std::size_t> rpoNumber;
+  Set<BasicBlock *> visited;
+  Vec<BasicBlock *> reversePostOrder;
+
+  // compute post order
+  std::function<void(BasicBlock *)> postorder = [&](BasicBlock *root) {
+    visited.insert(root);
+    for (auto *succ : root->getSuccessors()) {
+      if (visited.find(succ) == visited.end()) {
+        postorder(succ);
+      }
+    }
+    reversePostOrder.push_back(root);
+  };
+  postorder(f.getEntryBlock());
+  visited.clear();
+
+  // reverse it
+  std::reverse(reversePostOrder.begin(), reversePostOrder.end());
+  for (std::size_t i = 0; i < reversePostOrder.size(); ++i) {
+    rpoNumber[reversePostOrder[i]] = i + 1;
+  }
+
+  auto intersect = [&](BasicBlock *b1, BasicBlock *b2) {
+    auto *finger1 = b1;
+    auto *finger2 = b2;
+    while (rpoNumber[finger1] != rpoNumber[finger2]) {
+      while (rpoNumber[finger1] > rpoNumber[finger2]) {
+        finger1 = idoms[finger1];
+      }
+      while (rpoNumber[finger2] > rpoNumber[finger1]) {
+        finger2 = idoms[finger2];
+      }
+    }
+    return finger1;
+  };
+
+  bool changed = true;
+  while (changed) {
+    changed = false;
+
+    for (auto *curr : reversePostOrder) {
+      if (curr == f.getEntryBlock()) {
+        continue;
+      }
+
+      // Find the predecessor with the lowest RPO number
+      BasicBlock *lowest = curr->getPredecessors().front();
+      auto lowestNumber = rpoNumber[lowest];
+      for (auto *pred : curr->getPredecessors()) {
+        auto num = rpoNumber[pred];
+        if (num < lowestNumber) {
+          lowestNumber = num;
+          lowest = pred;
+        }
+      }
+
+      // Process the remaining predecessors
+      BasicBlock *newIdom = lowest;
+      for (auto *pred : curr->getPredecessors()) {
+        if (lowest == pred) {
+          continue;
+        }
+        if (idoms[pred] != nullptr) {
+          newIdom = intersect(pred, newIdom);
+        }
+      }
+      if (idoms[curr] != newIdom) {
+        idoms[curr] = newIdom;
+        changed = true;
+      }
+    }
+  }
+
+  Vec<String> labels;
+  for (auto *block : basicBlocks) {
+    labels.push_back(block->getLabel());
+  }
+  auto domTree = Make<DominatorTree>(labels);
+
+  for (auto &[b1, b2] : idoms) {
+    if (b1 == f.getEntryBlock()) {
+      continue;
+    }
+    domTree->addChild(b2->getLabel(), b1->getLabel());
+  }
+
   computeDominators();
   computeDominanceFrontier();
   computeDominatorTree();
+
+  std::cout << *domTree << std::endl;
+  std::cout << *dominatorTree << std::endl;
 }
 
 void DominatorAnalysis::computeDominators() {
